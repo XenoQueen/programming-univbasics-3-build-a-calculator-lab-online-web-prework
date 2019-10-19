df1e069cf69c1299c0f8b675d3f5fa5ffda6c069
Maika Kailani <queenmab051@gmail.com>
2019-10-19 20:29:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,29 @@
 def addition(num1, num2)
-
+num1 = 4
+num2 = 8
+  puts #{num1 + num2}
 end
 
 def subtraction(num1, num2)
-
+num1 = 12
+num2 = 5
+  puts #{num1 - num2}
 end
 
 def division(num1, num2)
-
+num1 = 8
+num2 = 2
+  puts #{num1 / num2}
 end
 
 def multiplication(num1, num2)
-
+num1 = 18
+num2 = 10
+  puts #{num1 * num2}
 end
 
 def modulo(num1, num2)
-
+num1 = 
 end
 
 def square_root(num)
