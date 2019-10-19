420e85fe1b20b48b55de7e753fdbfd0c6a6cc812
Maika Kailani <queenmab051@gmail.com>
2019-10-19 20:42:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -2,25 +2,28 @@ def addition(num1, num2)
 num1 = 4
 num2 = 8
   expect(addition(4,8)).to eq(12)
-  puts #{num1 + num2}
+  end
 end
 
 def subtraction(num1, num2)
 num1 = 12
 num2 = 5
-  puts #{num1 - num2}
+  expect(subtraction(12,5)).to eq(7)
+  end
 end
 
 def division(num1, num2)
 num1 = 8
 num2 = 2
-  puts #{num1 / num2}
+  expect(division(8,2)).to eq(4)
+  end
 end
 
 def multiplication(num1, num2)
 num1 = 18
 num2 = 10
-  puts #{num1 * num2}
+  expect(multiplication(18,10)).to eq(180)
+  end
 end
 
 def modulo(num1, num2)
