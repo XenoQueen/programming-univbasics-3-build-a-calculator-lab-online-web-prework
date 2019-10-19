e9b2c3aae8ecc4ebc171e0ee88ac15424c33cc65
Maika Kailani <queenmab051@gmail.com>
2019-10-19 20:44:52 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,40 @@
 def addition(num1, num2)
-
+num1 = 4
+num2 = 8
+  expect(addition(4,8)).to eq(12)
+  end
 end
 
 def subtraction(num1, num2)
-
+num1 = 12
+num2 = 5
+  expect(subtraction(12,5)).to eq(7)
+  end
 end
 
 def division(num1, num2)
-
+num1 = 8
+num2 = 2
+  expect(division(8,2)).to eq(4)
+  end
 end
 
 def multiplication(num1, num2)
-
+num1 = 18
+num2 = 10
+  expect(multiplication(18,10)).to eq(180)
+  end
 end
 
 def modulo(num1, num2)
-
+num1 = 20
+num2 = 8
+  expect(modulo(20,8)).to eq(2.5)
+  end
 end
 
 def square_root(num)
-
+num = 49
+  expect(square_root(49)).to eq(7)
+  end
 end
